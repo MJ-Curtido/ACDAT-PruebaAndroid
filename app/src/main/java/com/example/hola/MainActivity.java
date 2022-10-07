e25c91d9324aa39adb54ea0b04ebcc520adf557a
MJ-Curtido <manu17pc@gmail.com>
2022-10-07 12:57:37 +0200

    Intento de lo de lista

--- app/src/main/java/com/example/hola/MainActivity.java
+++ app/src/main/java/com/example/hola/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.hola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.hola.R;
+
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+    private static Integer cont = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnSaludar2 = findViewById(R.id.btnSaludar2);
+        Button btnSaludar = findViewById(R.id.btnSaludar);
+        Button btnContar = findViewById(R.id.btnContar);
+
+        btnSaludar2.setOnClickListener(this);
+        btnContar.setOnClickListener(this);
+
+        Log.i("info", "funciona");
+
+        /*
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        TextView texto = new TextView(this);
+        texto.setText("Hello, Android");
+        setContentView(texto);
+        */
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView txtSaludo = findViewById(R.id.txtSaludo);
+
+        if (view.getId() == R.id.btnSaludar){
+            txtSaludo.setText("wenos dia");
+        }
+        else if (view.getId() == R.id.btnSaludar2){
+            txtSaludo.setText("wenas tarde");
+        }
+        else {
+            cont++;
+            Button btnContar = (Button) view;
+            btnContar.setText("Pulsaciones: " + cont);
+        }
+        //Toast.makeText(this, "Hehehe shuuuuuu", Toast.LENGTH_LONG).show();
     }
 }
