f1cea56c1897b5c7e4c5242448f164dfae1d53be
MJ-Curtido <manu17pc@gmail.com>
2022-10-14 12:50:32 +0200

    Funciona

--- app/src/main/java/com/example/hola/Calculadora.java
+++ app/src/main/java/com/example/hola/Calculadora.java
@@ -3,6 +3,10 @@ package com.example.hola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.hola.calculadora.ListenerBotones;
 
 public class Calculadora extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class Calculadora extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+        new ListenerBotones(this);
+    }
+
+    public void hacerSuma() {
+        EditText txtNum1 = findViewById(R.id.txtNum1);
+        EditText txtNum2 = findViewById(R.id.txtNum2);
+        TextView txtSolucion = findViewById(R.id.txtSolucion);
+
+        if (!txtNum1.getText().toString().equals("") && !txtNum2.getText().toString().equals("")) {
+            txtSolucion.setText("Resultado: " + (Double.parseDouble(txtNum1.getText().toString()) + Double.parseDouble(txtNum2.getText().toString())));
+        }
+        else {
+            txtSolucion.setText("Hay algún campo sin completar");
+        }
+    }
+
+    public void hacerResta() {
+        EditText txtNum1 = findViewById(R.id.txtNum1);
+        EditText txtNum2 = findViewById(R.id.txtNum2);
+        TextView txtSolucion = findViewById(R.id.txtSolucion);
+
+        if (!txtNum1.getText().toString().equals("") && !txtNum2.getText().toString().equals("")) {
+            txtSolucion.setText("Resultado: " + (Double.parseDouble(txtNum1.getText().toString()) - Double.parseDouble(txtNum2.getText().toString())));
+        }
+        else {
+            txtSolucion.setText("Hay algún campo sin completar");
+        }
     }
 }
--- app/src/main/java/com/example/hola/calculadora/ListenerBotones.java
+++ app/src/main/java/com/example/hola/calculadora/ListenerBotones.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import com.example.hola.Calculadora;
 import com.example.hola.R;
 
-public abstract class ListenerBotones implements View.OnClickListener {
+public class ListenerBotones implements View.OnClickListener {
     Calculadora calc;
 
     public ListenerBotones(Calculadora calc) {
@@ -20,20 +20,11 @@ public abstract class ListenerBotones implements View.OnClickListener {
     }
 
     public void onClick(View view) {
-        EditText txtNum1 = calc.findViewById(R.id.txtNum1);
-        EditText txtNum2 = calc.findViewById(R.id.txtNum2);
-
-        TextView txtSolucion = calc.findViewById(R.id.txtSolucion);
-        if (!txtNum1.getText().toString().equals("") && !txtNum2.getText().toString().equals("")) {
-            if (view.getId() == R.id.btnSumar) {
-                txtSolucion.setText("Resultado: " + (Double.parseDouble(txtNum1.getText().toString()) + Double.parseDouble(txtNum2.getText().toString())));
-            }
-            else {
-                txtSolucion.setText("Resultado: " + (Double.parseDouble(txtNum1.getText().toString()) - Double.parseDouble(txtNum2.getText().toString())));
-            }
+        if (view.getId() == R.id.btnSumar) {
+            calc.hacerSuma();
         }
         else {
-            txtSolucion.setText("Hay algún campo sin completar");
+            calc.hacerResta();
         }
     }
 }
